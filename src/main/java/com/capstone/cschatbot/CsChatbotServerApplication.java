2ed55cbdfcdedacced5cd4dcbad2862b98fd16b6
박성춘 <songchoon10@naver.com>
2024-04-10 18:31:53 +0900

    first commit

---
+++ src/main/java/com/capstone/cschatbot/CsChatbotServerApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.cschatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsChatbotServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsChatbotServerApplication.class, args);
+	}
+
+}
