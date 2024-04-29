47d7be77488bf528ea0f3c5ffa62bb7cac2966a7
박성춘 <songchoon10@naver.com>
2024-04-29 14:24:58 +0900

    oauth 진행중

--- src/main/java/com/capstone/cschatbot/member/service/AuthServiceImpl.java
+++ src/main/java/com/capstone/cschatbot/member/service/AuthServiceImpl.java
@@ -3,11 +3,13 @@ package com.capstone.cschatbot.member.service;
 import com.capstone.cschatbot.member.repository.MemberRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
+@Slf4j
 public class AuthServiceImpl implements AuthService{
     private final MemberRepository memberRepository;
 }
