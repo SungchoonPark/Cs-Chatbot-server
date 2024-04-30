ebf20359db59365e2b1a57ce136f3f048b57c166
박성춘 <songchoon10@naver.com>
2024-04-30 20:58:29 +0900

    refactor: 빌더패턴을 적용

--- src/main/java/com/capstone/cschatbot/config/oauth/dto/OIDCDecodePayload.java
+++ src/main/java/com/capstone/cschatbot/config/oauth/dto/OIDCDecodePayload.java
@@ -1,9 +1,11 @@
 package com.capstone.cschatbot.config.oauth.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class OIDCDecodePayload {
     /** issuer ex) https://kauth.kakao.com */
