8b7e6b2fddf95abff0b8b5c442f40e1e5167797f
박성춘 <songchoon10@naver.com>
2024-04-30 20:56:31 +0900

    feat: feign client를 이용한 카카오 공개키 목록 조회 및 로그인, 강제 회원가입 로직 기능 구현

--- src/main/java/com/capstone/cschatbot/config/oauth/OauthProperties.java
+++ src/main/java/com/capstone/cschatbot/config/oauth/OauthProperties.java
@@ -8,7 +8,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
+@Setter
 @RequiredArgsConstructor
+@AllArgsConstructor
 @Configuration
 @ConfigurationProperties("oauth")
 public class OauthProperties {
