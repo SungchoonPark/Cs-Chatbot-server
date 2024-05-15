5f60dc78dfb670a7145e0c4f3e919dc75c7c18cb
박성춘 <songchoon10@naver.com>
2024-05-15 14:24:11 +0900

    remove: 리팩토링 과정에서 필요없어진 클래스 파일들 삭제

--- src/main/java/com/capstone/cschatbot/chat/repository/ChatRepository.java
+++
@@ -1,8 +0,0 @@
-package com.capstone.cschatbot.chat.repository;
-
-import com.capstone.cschatbot.chat.entity.CSChat;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ChatRepository extends MongoRepository<CSChat, String> {
-}
-
