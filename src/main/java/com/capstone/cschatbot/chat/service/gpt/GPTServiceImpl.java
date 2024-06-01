e77b2b17f07d43e504eee4f68d0beb5637856dac
박성춘 <songchoon10@naver.com>
2024-06-01 15:31:54 +0900

    refactor: RestTemplate to RestClient

--- src/main/java/com/capstone/cschatbot/chat/service/gpt/GPTServiceImpl.java
+++ src/main/java/com/capstone/cschatbot/chat/service/gpt/GPTServiceImpl.java
@@ -6,27 +6,18 @@ import com.capstone.cschatbot.common.enums.CustomResponseStatus;
 import com.capstone.cschatbot.common.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class GPTServiceImpl implements GPTService {
-    @Value("${openai.url}")
-    private String apiUrl;
-
-    @Qualifier("gptRestTemplate")
-    @Autowired
-    private final RestTemplate gptRestTemplate;
+    private final GPTComponent gptComponent;
 
     @Override
     public String getNewQuestion(ChatRequest chatRequest) {
-        ChatResponse gptResponse = gptRestTemplate.postForObject(apiUrl, chatRequest, ChatResponse.class);
+        ChatResponse gptResponse = gptComponent.getGptResponse(chatRequest);
         checkValidGptResponse(gptResponse);
 
         return gptResponse.getChoices().get(0).getMessage().getContent();
